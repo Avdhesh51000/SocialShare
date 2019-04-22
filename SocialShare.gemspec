4cbff17aff9a4f85d0c4c3a68991236f2fc08fb0
Avdhesh <avdhesh51000@gmail.com>
2019-04-22 15:00:08 +0530

    for Ruby gems

--- SocialShare.gemspec
+++ SocialShare.gemspec
@@ -11,16 +11,16 @@ Gem::Specification.new do |spec|
 
   spec.summary       = %q{Social Share on networks and shared count.}
   spec.description   = %q{Share on social media platform and check how many times url was shared in social networks, e.g. share counts.}
-  # spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/Avdhesh51000/SocialShare"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
+    spec.metadata["allowed_push_host"] = "https://rubygems.org/"
     # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-
-    # spec.metadata["homepage_uri"] = spec.homepage
-    # spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
+    spec.metadata["homepage_uri"] = spec.homepage
+    spec.metadata["source_code_uri"] = "https://github.com/Avdhesh51000/SocialShare"
     # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
   else
     raise "RubyGems 2.0 or newer is required to protect against " \
--- lib/share_count.rb
+++ lib/share_count.rb
@@ -1,5 +1,5 @@
- require 'rest-client'
- require 'json'
+require 'rest-client'
+require 'json'
 require 'share_count/string_helper'
 require 'share_count/base'
 require 'share_count/facebook'
@@ -11,88 +11,88 @@ require 'share_count/twitter'
 require 'share_count/weibo'
 module ShareCount
   # class << self
-    include ShareCount::StringHelper
-    def config=(val)
-      ShareCount::Base.config = val
-    end
-    NETWORK_LIST = [
-      :facebook,
-      :google,
-      :linkedin,
-      :pinterest,
-      :reddit,
-      :twitter,
-      :weibo
-    ]
-    def supported_networks
-      NETWORK_LIST
-    end
-    NETWORK_LIST.each do |network_name|
-      define_method(network_name) do |url|
-        class_name = to_camel_case(network_name.to_s)
-        ShareCount.const_get(class_name).new(url).shares
-      end
-      define_method("#{network_name}!") do |url|
-        class_name = to_camel_case(network_name.to_s)
-        ShareCount.const_get(class_name).new(url).shares!
-      end
-    end
-    def omit(url, excluded_networks = [])
-      selected_base(url, NETWORK_LIST.map(&:to_s) - excluded_networks.map(&:to_s), false)
-    end
-    def omit!(url, excluded_networks = [])
-      selected_base(url, NETWORK_LIST.map(&:to_s) - excluded_networks.map(&:to_s), true)
-    end
-    def selected(url, selected_networks)
-      selected_base(url, selected_networks, false)
-    end
-    def selected!(url, selected_networks)
-      selected_base(url, selected_networks, true)
-    end
-    def all(url)
-      selected(url, NETWORK_LIST)
-    end
-    def all!(url)
-      selected!(url, NETWORK_LIST)
-    end
-    def total(url, selected_networks = NETWORK_LIST)
-      selected!(url, selected_networks).values.reduce(:+)
-    end
-    def has_any?(url, selected_networks = NETWORK_LIST)
-      lambdas = filtered_networks(selected_networks).map do |network_name|
-        -> {self.send("#{network_name}!", url)}
-      end
-      pool = thread_pool(lambdas)
-      found = false
-      while !pool.empty? && !found
-        sleep 0.05
-        if thread = pool.find(&:stop?)
-          if thread.value > 0
-            pool.each(&:kill)
-            found = true
-          else
-            pool.delete(thread)
-          end
-        end
-      end
-      found
-    end
-  private
-    def filtered_networks(selected_networks)
-      selected_networks.map(&:to_s) & NETWORK_LIST.map(&:to_s)
-    end
-    def thread_pool(lambdas)
-      lambdas.map { |l| Thread.new { l.call } }
-    end
-      def selected_base(url, selected_networks, with_exception)
-        lambdas = filtered_networks(selected_networks).map do |network_name|
-          method_name = with_exception ? "#{network_name}!" : network_name
-          -> {{network_name => self.send(method_name, url)}}
-        end
-        thread_pool(lambdas).reduce({}) do |result, thread|
-          thread.join
-          result.merge(thread.value)
-        end
-      end
-    # end
+  include ShareCount::StringHelper
+  def config=(val)
+    ShareCount::Base.config = val
   end
+  NETWORK_LIST = [
+    :facebook,
+    :google,
+    # :linkedin,
+    # :pinterest,
+    :reddit,
+    # :twitter,
+    :weibo
+  ]
+  def supported_networks
+    NETWORK_LIST
+  end
+  NETWORK_LIST.each do |network_name|
+    define_method(network_name) do |url|
+      class_name = to_camel_case(network_name.to_s)
+      ShareCount.const_get(class_name).new(url).shares
+    end
+    define_method("#{network_name}!") do |url|
+      class_name = to_camel_case(network_name.to_s)
+      ShareCount.const_get(class_name).new(url).shares!
+    end
+  end
+  def omit(url, excluded_networks = [])
+    selected_base(url, NETWORK_LIST.map(&:to_s) - excluded_networks.map(&:to_s), false)
+  end
+  def omit!(url, excluded_networks = [])
+    selected_base(url, NETWORK_LIST.map(&:to_s) - excluded_networks.map(&:to_s), true)
+  end
+  def selected(url, selected_networks)
+    selected_base(url, selected_networks, false)
+  end
+  def selected!(url, selected_networks)
+    selected_base(url, selected_networks, true)
+  end
+  def all(url)
+    selected(url, NETWORK_LIST)
+  end
+  def all!(url)
+    selected!(url, NETWORK_LIST)
+  end
+  def total(url, selected_networks = NETWORK_LIST)
+    selected!(url, selected_networks).values.reduce(:+)
+  end
+  def has_any?(url, selected_networks = NETWORK_LIST)
+    lambdas = filtered_networks(selected_networks).map do |network_name|
+      -> {self.send("#{network_name}!", url)}
+    end
+    pool = thread_pool(lambdas)
+    found = false
+    while !pool.empty? && !found
+      sleep 0.05
+      if thread = pool.find(&:stop?)
+        if thread.value > 0
+          pool.each(&:kill)
+          found = true
+        else
+          pool.delete(thread)
+        end
+      end
+    end
+    found
+  end
+  private
+  def filtered_networks(selected_networks)
+    selected_networks.map(&:to_s) & NETWORK_LIST.map(&:to_s)
+  end
+  def thread_pool(lambdas)
+    lambdas.map { |l| Thread.new { l.call } }
+  end
+  def selected_base(url, selected_networks, with_exception)
+    lambdas = filtered_networks(selected_networks).map do |network_name|
+      method_name = with_exception ? "#{network_name}!" : network_name
+      -> {{network_name => self.send(method_name, url)}}
+    end
+    thread_pool(lambdas).reduce({}) do |result, thread|
+      thread.join
+      result.merge(thread.value)
+    end
+  end
+  # end
+end
